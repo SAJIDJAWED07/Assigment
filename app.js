0d3d6ff985ceb5a1e968d8d5781b968731a049f0
SAJID JAWED <sajidjawed206080@gmail.com>
2023-12-24 14:43:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+  /////**** ANSWER NO 3 *****/////
+// const capitalizeWords = (sentence) => {
+//     const words = sentence.split(' ');
+//     const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     const capsent = capitalizedWords.join(' ');
+//     return capsent;
+// };
+// const Name = "muhammad sajid";
+// const result = capitalizeWords(Name);
+// console.log(`Old: "${Name}"`);
+// console.log(`Result: "${result}"`);
+
+
+  /////**** ANSWER NO 6 *****/////
+// const myArray = [1, 2, 3, 4, 5, 3, 6];
+// const targetElement = 3;
+// const index = myArray.indexOf(targetElement);
+// if (index !== -1) {
+//   console.log(`The first occurrence of ${targetElement} is at index ${index}`);
+// } else {
+//   console.log(`${targetElement} array ma nahi ha`);
+// }
+
+
+   /////**** ANSWER NO 7 *****/////
+// function removeDuplicates(inputArray) {
+//     var newArray = [];
+//     for (var i = 0; i < inputArray.length; i++) {
+//         if (newArray.indexOf(inputArray[i]) === -1) {
+//             newArray.push(inputArray[i]);
+//         }
+//     }
+//     return newArray;
+// }
+// var originalArray = [1, 2, 3, 4, 3, 2, 5];
+// var newArrayWithoutDuplicates = removeDuplicates(originalArray);
+// console.log(newArrayWithoutDuplicates);
+
+
+  /////**** ANSWER NO 9 *****/////
+// var number = 2;
+// while (number <= 20) {
+//   console.log(number);
+//   number += 2;
+// }
